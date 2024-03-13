66c739f0a412c32536d4f0090ff12144732ef629
PeamMiiZ <peemmygg@gmail.com>
2024-03-13 20:34:27 +0700

    Update The Feature or the codee ( doesn't exist)

--- Homework2_PipeChat/chat.c
+++ Homework2_PipeChat/chat.c
@@ -85,7 +85,7 @@ int main(int argc , char *argv[]) {
                 break; 
             case 0 : 
                 while(strncmp(rbuf , "end chat" , 8)) {
-
+                    
                 }
                 break; 
             default : 
