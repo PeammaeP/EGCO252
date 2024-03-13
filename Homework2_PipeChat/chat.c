26caf1a8d9d3d5772be46f8b09b156e1210c0921
PeamMiiZ <peemmygg@gmail.com>
2024-03-13 22:18:11 +0700

    Success the Program 1

--- Homework2_PipeChat/chat.c
+++ Homework2_PipeChat/chat.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+#include <signal.h>
 
 #define FIFO_1 "./fifo1to2"
 #define FIFO_2 "./fifo2to1"
@@ -11,6 +13,10 @@
 #define MAX_RBUF 80
 
 int FIFO_FD1 , FIFO_FD2; 
+
+static void sig_end() {
+    exit(EXIT_SUCCESS); 
+}
 
 int main(int argc , char *argv[]) {
     int child , nbytes; 
@@ -30,7 +36,7 @@ int main(int argc , char *argv[]) {
         FIFO_FD1 = mkfifo(FIFO_1 , 0777); 
 
         if(FIFO_FD1) {
-            fprintf(stderr , "Could not create the first file with %s\n",FIFO_FD1);
+            fprintf(stderr , "Could not create the first file with %s\n",FIFO_1);
             exit(EXIT_FAILURE); 
         }
     }
@@ -39,62 +45,88 @@ int main(int argc , char *argv[]) {
         FIFO_FD2 = mkfifo(FIFO_2 , 0777); 
         
         if(FIFO_FD2) {
-            fprintf(stderr , "Could not create the second file with %s\n",FIFO_FD2);
+            fprintf(stderr , "Could not create the second file with %s\n",FIFO_2);
             exit(EXIT_FAILURE); 
         }
     }
 
     /* If the file was created ( FIFO_FD1 and FIFO_FD2 ) then open file within the below code.
     The both files permission was set in the read only mode. */
-    FIFO_FD1 = open(FIFO_1 , O_WRONLY);
-    FIFO_FD2 = open(FIFO_2 , O_WRONLY);
 
     // Avoid to Read the Name File ( That was not relevant with this code )
     argv++; 
     
     if(strcmp(*argv , "1") == 0) {
-        pid_t pid;
-        pid = fork(); 
+        FIFO_FD1 = open(FIFO_1 , O_WRONLY);
+        FIFO_FD2 = open(FIFO_2 , O_RDONLY);
 
-        switch(pid) {
-            case -1 : 
-                perror("Forking Failed !");
-                exit(EXIT_FAILURE); 
-                break;
-            case 0 : 
-                while(strncmp(rbuf , "end chat" , 8)) {
+        child = fork(); 
 
-                }
-
-                break;
-            default : 
-                while(strncmp(rbuf , "end chat" , 8)) {
-
-                }
-        }
-    }
-
-    if(strcmp(*argv , "2" == 0)) {
-        pid_t pid;
-        pid = fork(); 
-
-        switch(pid) {
+        switch(child) {
             case -1 :
                 perror("Could not Fork , Process was failed ");
                 exit(EXIT_FAILURE);
                 break; 
             case 0 : 
+                // Child Process 
                 while(strncmp(rbuf , "end chat" , 8)) {
-                    
+                    if((nbytes = read(FIFO_FD2 , rbuf , MAX_RBUF)) > 0 ) {
+                        printf("User 2 : \n");
+                        write(1,rbuf,nbytes);
+                    }
                 }
                 break; 
             default : 
+                // Parent Process 
                 while(strncmp(rbuf , "end chat" , 8)) {
-
+                    if((nbytes = read(0,rbuf,MAX_RBUF)) > 0) {
+                        write(FIFO_FD1,rbuf,nbytes);
+                    }
                 }
         }
+        printf("\n");
+    }
+
+    if(strcmp(*argv , "2") == 0) {
+        FIFO_FD1 = open(FIFO_1 , O_RDONLY);
+        FIFO_FD2 = open(FIFO_2 , O_WRONLY);
+
+        child = fork(); 
+
+        switch(child) {
+            case -1 :
+                perror("Could not Fork , Process was failed ");
+                exit(EXIT_FAILURE);
+                break; 
+            case 0 : 
+            // Child Process 
+                while(strncmp(rbuf, "end chat" , 8)) {
+                    if((nbytes = read(FIFO_FD1 , rbuf , MAX_RBUF)) > 0) {
+                        printf("User 1 : \n");
+                        write(1,rbuf,nbytes);
+                    }
+                }
+                break; 
+            default : 
+            // Parent Process 
+                while(strncmp(rbuf , "end chat" , 8)) {
+                    if((nbytes = read(0,rbuf,MAX_RBUF)) > 0) {
+                        write(FIFO_FD2,rbuf,nbytes);
+                    }
+                }
+        }
+        printf("\n");
     } 
 
+    if(child > 0) {
+        // Parent Process -> Call the function sig_end 
+        kill(child , SIGUSR1);
+    }
+    else if(child == 0) {
+        // Child Process -> Call the function sig_end 
+        kill(getppid() , SIGUSR1); 
+    }
+ 
     if(FIFO_FD1 != -1) {
         close(FIFO_FD1);
     }
